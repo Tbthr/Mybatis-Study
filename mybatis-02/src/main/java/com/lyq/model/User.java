ff50df958a6b159f5b58968105e6c585efa6c25b
Tbthr <1598622578@qq.com>
2020-03-02 13:39:58 +0800

    xml配置

---
+++ mybatis-02/src/main/java/com/lyq/model/User.java
@@ -0,0 +1,19 @@
+package com.lyq.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+// @Alias("_user") //设置别名
+public class User {
+    
+    private int id;  //id
+    private String name;   //姓名
+    private String pwd;   //密码
+    
+}
---
+++ mybatis-02/src/test/java/model/UserTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import com.lyq.mapper.UserMapper;
+import com.lyq.model.User;
+import com.lyq.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author lyq_power
+ * @date 2020/2/28 9:37 09s
+ */
+public class UserTest {
+    @Test
+    public void selectUser() {
+        SqlSession session = MybatisUtils.getSession();
+        //方法一: 旧
+//        List<User> users = session.selectList("com.com.lyq.mapper.UserMapper.selectUser");
+
+        //方法二: 新
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        List<User> users = mapper.selectUser();
+
+        for (User user: users){
+            System.out.println(user);
+        }
+        session.close();
+    }
+}
